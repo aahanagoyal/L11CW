1042218f3ae2caddfdeb84134f000383fe2ec0a3
aahanagoyal <neha108425@gmail.com>
2026-01-06 21:03:22 +0530

    Add files via upload

---
+++ a2.py
@@ -0,0 +1,8 @@
+r=int(input("Enter the number of rows:"))
+num=1
+print("Floyd's triangle.")
+for i in range(1,r+1):
+    for j in range(1,i+1):
+        print(num,end=" ")
+        num=num+1
+    print()
